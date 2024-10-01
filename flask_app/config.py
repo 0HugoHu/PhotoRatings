c034bc17a7c4184fec0adcc67a5400ff21cfca79
0HugoHu <0@hugohu.top>
2024-09-30 23:41:04 -0700

    Feat: add scheduler for preprocessing image sizes

--- flask_app/config.py
+++ flask_app/config.py
@@ -9,6 +9,8 @@ RAW_FOLDER = os.path.join(BASE_DIR, 'images_raw')
 UNRATED_FOLDER = os.path.join(BASE_DIR, 'images_unrated')
 RATED_FOLDER = os.path.join(BASE_DIR, 'images_rated')
 DEBUG_FOLDER = os.path.join(BASE_DIR, 'images_debug')
+PREPROCESS_FOLDER = os.path.join(BASE_DIR, 'images_preprocessed')
+METADATA_FILE= os.path.join(PREPROCESS_FOLDER, "metadata.jsonl")
 LOG_FILE = os.path.join(BASE_DIR, 'logs', 'images_log.json')
 OPERATION_LOG_FILE = os.path.join(BASE_DIR, 'logs', 'operations.log')
 LOG_ARCHIVE_FOLDER = os.path.join(BASE_DIR, 'logs_archive')
@@ -23,11 +25,17 @@ SERVE_TIMEOUT_SECONDS = 300  # seconds (5 minutes)
 SCHEDULER_INTERVAL_MOVE_TO_UNRATED = 1 * 60 * 60  # (hour)
 SCHEDULER_INTERVAL_CHECK_LOG_SIZE = 0.5 * 60 * 60 # (hour)
 SCHEDULER_INTERVAL_CREATE_THUMBNAILS = 1 * 60 * 60 # (hour)
+SCHEDULER_INTERVAL_PREPROCESSING_IMAGE = 24 * 60 * 60 # (hour)
 MAX_LOG_FILE_SIZE_BYTES = 20 * 1024 * 1024  # MB
 # ---------------- Image compression ----------------
 MAX_IMAGE_SIZE_BYTES = 2 * 1024 * 1024  # MB
 Image.MAX_IMAGE_PIXELS = 933120000
 IMAGE_THUMBNAIL_SIZE = 1024
+IMAGE_PREPROCESS_SIZE = 1024
+# ---------------- Score ----------------
+SCORE_MAP = {
+    "-1": 0.0, "0": 0.0, "1": 0.2, "2": 0.4, "3": 0.6, "4": 0.8, "5": 1.0
+}
 
 # Configure logging
 logging.basicConfig(
@@ -46,6 +54,6 @@ if not os.path.exists(LOG_FILE):
     with open(LOG_FILE, 'w') as log_file:
         json.dump({}, log_file)
 
-for folder in [RAW_FOLDER, UNRATED_FOLDER, RATED_FOLDER, DEBUG_FOLDER, LOG_ARCHIVE_FOLDER]:
+for folder in [RAW_FOLDER, UNRATED_FOLDER, RATED_FOLDER, DEBUG_FOLDER, LOG_ARCHIVE_FOLDER, PREPROCESS_FOLDER]:
     if not os.path.exists(folder):
         os.makedirs(folder, exist_ok=True)
--- flask_app/schedulers.py
+++ flask_app/schedulers.py
@@ -14,9 +14,11 @@ def start_scheduler():
     job1 = scheduler.add_job(move_images_to_unrated, 'interval', seconds=SCHEDULER_INTERVAL_MOVE_TO_UNRATED)
     job2 = scheduler.add_job(archive_log_file, 'interval', seconds=SCHEDULER_INTERVAL_CHECK_LOG_SIZE) 
     job3 = scheduler.add_job(create_thumbnail, 'interval', seconds=SCHEDULER_INTERVAL_CREATE_THUMBNAILS) 
+    job4 = scheduler.add_job(preprocess_image_size, 'interval', seconds=SCHEDULER_INTERVAL_PREPROCESSING_IMAGE)
     job1.func()
     job2.func()
     job3.func()
+    job4.func()
     scheduler.start()
 
 
@@ -100,4 +102,44 @@ def create_thumbnail():
                         log_operation(f"Failed to create thumbnail for {file}: {str(e)}")
     
     log_operation(f"Finished creating thumbnails. Time taken: {time.time() - start_time:.2f} seconds.")
+
+
+def preprocess_image_size():
+    start_time = time.time()
+    log_operation("Starting to preprocess for desired photo size.")
+    
+    partition_folders = [f for f in os.listdir(RATED_FOLDER)]
+    output_folder = PREPROCESS_FOLDER
+    file_counter = 1
+
+    with open(METADATA_FILE, 'w') as metadata_file:
+        for partition in partition_folders:
+            partition_folder = os.path.join(RATED_FOLDER, partition)
+            target_score = SCORE_MAP.get(str(partition), 0.0) 
+
+            for file in os.listdir(partition_folder):
+                if file.lower().endswith(('.jpg', '.jpeg', '.png')):
+                    source_path = os.path.join(partition_folder, file)
+                    
+                    file_extension = os.path.splitext(file)[1].lower()
+                    output_filename = f"{file_counter:05d}{file_extension}"
+                    preprocessing_path = os.path.join(output_folder, output_filename)
+                    
+                    try:
+                        with Image.open(source_path) as img:
+                            img.thumbnail((IMAGE_PREPROCESS_SIZE, IMAGE_PREPROCESS_SIZE))
+                            img.save(preprocessing_path)
+                            
+                            metadata = {
+                                "file_name": output_filename,
+                                "target": target_score
+                            }
+                            metadata_file.write(json.dumps(metadata) + "\n")
+                            file_counter += 1
+                            log_operation(f"Preprocessed image {source_path} to {preprocessing_path}, score: {target_score}")
+                        
+                    except Exception as e:
+                        log_operation(f"Failed to create thumbnail for {file}: {str(e)}")
+    
+    log_operation(f"Finished resizing images. Time taken: {time.time() - start_time:.2f} seconds.")
 
